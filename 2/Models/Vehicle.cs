9c1ebd65a74ac64d892015f9ca70085a2de7e943
madataliyev <madatma-apa202@code.edu.az>
2026-03-17 18:29:29 +0400

    ders tapsiriq

---
+++ 2/Models/Vehicle.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Models
+{
+    internal class Vehicle
+    {
+        private string _brand;
+        public string Brand
+        { 
+            get
+            {
+                return _brand;
+            }
+            set
+            {
+                if( value.Length>3)
+                {
+                    _brand = value;
+                }
+                else
+                {
+                    Console.WriteLine(" Minimum 3 simvol olmalidir!");
+                }
+            }
+        }
+
+
+        private  string _model;
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                if ( value.Length>3)
+                {
+                    _model = value;
+                }
+                else
+                {
+                    Console.WriteLine("Minimum 1 simvol olmalidir!");
+                }
+            }
+
+        }
+
+        private int  _year;
+        public int Year
+        {
+            get
+            {
+                return _year;
+            }
+            set
+            {
+                if (value >= 1900)
+                {
+                    _year = value;
+                }
+                else
+                {
+                    Console.WriteLine("Minimum");
+                }
+            }
+        }
+        
+        public int  MileageKm { get; set; }
+        public bool IsRunning { get; set; }
+        public Vehicle(string brand, string model, int year)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.Year = year;
+
+        }
+        
+        public bool StartEngine()
+        {
+            IsRunning = true;
+            return true;
+        }
+        public void Drive (int km)
+        {
+            if (IsRunning==true)
+            {
+                MileageKm += km;
+            }
+            if (IsRunning==false)
+            {
+                Console.WriteLine(MileageKm);
+            }
+        }
+        public void VehicleInfo()
+        {
+            Console.WriteLine($" Brand; {Brand} ,Model; {Model}, Year: {Year}, Mileage:{MileageKm}, Running: {IsRunning}");
+            
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,7 @@
+﻿using _2.Models;
+using System.Xml.Schema;
+
+Vehicle vehicle1 = new();
+vehicle1.Brand = "t";
+Console.WriteLine(vehicle1.Brand);
+Console.WriteLine(vehicle1.Model);    
